107e4d2007f7ac506804cff0e2ded8ab1a9f284d
BlackSmokingOfMoon <104624954+tr1ma1d@users.noreply.github.com>
2023-09-13 22:26:45 +0300

    Add files via upload

---
+++ codewars2/codewars2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "the";
+            
+            Console.WriteLine(AlphabetPos(str));
+        }
+        static string AlphabetPos(string text)
+        {
+            return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c)).Select(x => x - 'a' + 1));
+            
+        }
+    }
+}
